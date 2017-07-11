55dc5895f20c9cf512e75da26f555447d6c82fae
BenHagenau <beha7507@colorado.edu>
2017-07-10 19:21:43 -0600

    Delete testredlayer.m

--- testredlayer.m
+++
@@ -1,8 +0,0 @@
-inImg=imread('lena1.jpg');
-[r,c,l]=size(inImg);
-outImg=zeros(r,c,3);
-
-outImg(:,:,1)=inImg(:,:,1);
-outImg(:,:,2)=inImg(:,:,2);
-outImg(:,:,3)=inImg(:,:,3);
-imshow(uint8(outImg))
