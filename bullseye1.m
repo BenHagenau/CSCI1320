0e2bc8d1d0ea0be7b6ad7478ec89bc026179400a
BenHagenau <beha7507@colorado.edu>
2017-07-10 19:12:18 -0600

    Delete bullseye1.m

--- bullseye1.m
+++
@@ -1,8 +0,0 @@
-function [ out ] = bullseye1(n)
-mat=zeros(n);
-for i=1:(n+1)/2
-    mat(i,(i:n-i+1))=((n+1)/2)-i+1;
-    mat((i:n-i+1),i)=((n+1)/2)-i+1;
-    mat((i:n-i+1),(i:n-i+1))=((n+1)/2)-i+1;
-end
-out=mat;
